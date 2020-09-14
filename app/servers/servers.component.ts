7d7fb21f90c1ed93b6da6713dc1649a0b7edbfcb
pradeepamatya <pradeepbdramatya@gmail.com>
2020-09-14 19:37:27 +0545

    propery binding

--- app/servers/servers.component.ts
+++ app/servers/servers.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit() {
+  }
 }
